cafe2227ea47b2e1e07b9b6a3252fc8eab8af670
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2023-09-02 19:48:40 +0530

    Time: 27 ms (19.35%), Space: 23.3 MB (5.64%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+bool solve(int i,string s, set<string> wd,vector<int>& dp) {
+    if(i>=s.size()) return true;
+    if(dp[i]!=-1)   return dp[i];
+    string t="";
+    for(int x=i;x<s.size();++x)
+    {
+        t+=s[x];
+        if(wd.find(t)!=wd.end() && solve(x+1,s,wd,dp))
+        {
+            dp[i]=1;
+            return true;
+        }
+            
+    }
+    dp[i]=0;
+    return false;
+}
+
+bool wordBreak(string s, vector<string>& wordDict) {
+        set<string> wd(wordDict.begin(),wordDict.end());
+        vector<int> dp(301,-1);
+        return solve(0,s,wd,dp);
+    }
+};
