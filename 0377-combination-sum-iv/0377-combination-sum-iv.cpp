c50c30ba8b6f501e33128895a96a006ae2d0915c
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2023-09-18 14:32:11 +0530

    Time: 21 ms (5.03%), Space: 7.8 MB (6.17%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -19,7 +19,7 @@ int solve(int ind,int sum,int& target,vector<int>& nums,vector<vector<int>>& dp)
     return dp[ind][sum]=count;
 }
     int combinationSum4(vector<int>& nums, int target) {
-        vector<vector<int>> dp(nums.size(),vector<int>(target+1,-1));
+        vector<vector<int>> dp(nums.size(),vector<int>(target,-1));
         return solve(0,0,target,nums,dp);
     }
 };
