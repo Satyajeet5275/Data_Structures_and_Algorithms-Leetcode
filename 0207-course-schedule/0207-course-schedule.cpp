6b9f909594502e7ce91a256cf9f15fe0328d7aad
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2023-10-01 01:39:29 +0530

    Time: 16 ms (73.83%), Space: 14.4 MB (38.48%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canFinish(int N, vector<vector<int>>& edge) {
+        vector<vector<int>> adj(N);
+	    vector<int> indegree(N,0);
+	    for(auto x:edge){
+	        int u=x[0];
+	        int v=x[1];
+	        indegree[v]++;
+	        adj[u].push_back(v);
+	    }
+	    queue<int> q;
+	    for(int i=0;i<N;i++){
+	        if(indegree[i]==0){
+	            q.push(i);
+	        }
+	    }
+	    vector<int> ans;
+	    while(!q.empty()){
+	        int node=q.front();
+	        q.pop();
+	        ans.push_back(node);
+	        for(auto x:adj[node]){
+	            indegree[x]--;
+	            if(indegree[x]==0){
+	                q.push(x);
+	            }
+	        }
+	    }
+	    if(ans.size()==N) return true;
+	    return false;
+    }
+};
