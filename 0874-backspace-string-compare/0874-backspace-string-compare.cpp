e9da4d43e860d344d262f49da642ed3d45e5fc2d
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2023-10-19 19:52:09 +0530

    Time: 0 ms (100%), Space: 6.8 MB (15.59%) - LeetHub

---
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string solve(string s){
+        int n=s.length();
+        string ans="";
+        for(int i=0;i<n;i++){
+            if(s[i]=='#'){
+                if(ans.size()!=0)
+                    ans.pop_back();
+                continue;
+            } 
+            ans+=s[i];
+        }
+        return ans;
+    }
+    bool backspaceCompare(string s, string t) {
+        string s1=solve(s);
+        string s2=solve(t);
+        if(s1==s2) return 1;
+        return 0;
+    }
+};
