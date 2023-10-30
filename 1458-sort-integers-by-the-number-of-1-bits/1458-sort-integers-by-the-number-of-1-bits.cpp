69e5aac4ca8013f99806d49450dcf1d2294cd05f
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2023-10-30 11:16:58 +0530

    Time: 168 ms (5.02%), Space: 10.5 MB (66.29%) - LeetHub

---
+++ 1458-sort-integers-by-the-number-of-1-bits/1458-sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    static int countBits(int n){
+        int bits=0;
+        while(n){
+            bits+=(n&1);
+            n>>=1;
+        }
+        return bits;
+    }
+    static bool comparator(int a,int b){
+        int aBits=countBits(a);
+        int bBits=countBits(b);
+        cout<<a<<"->"<<aBits<<endl;
+        cout<<b<<"->"<<bBits<<endl;
+        if(aBits==bBits)  return (a<b);
+        return (aBits<bBits);
+    }
+    vector<int> sortByBits(vector<int>& arr) {
+        sort(arr.begin(),arr.end(),comparator);
+        return arr;
+    }
+};
