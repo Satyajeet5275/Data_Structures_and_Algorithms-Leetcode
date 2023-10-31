dbc995c2f8e523578746661ebe537ffa75a33617
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2023-11-01 00:52:07 +0530

    Time: 16 ms (37.83%) | Memory: 20.1 MB (7.86%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -4,24 +4,13 @@ public:
         
         int size=nums.size();
         map<int,int> m;
-        
-        for(int i=0;i<size;i++)
-        {
-            m[nums[i]]=0;
-        }
-        
-        for(int i=0;i<size;i++)
-        {
-            m[nums[i]]++;
-        }
-        
+        for(int i=0;i<size;i++)    m[nums[i]]++;
+
         int lar=0;
         int loc;
         
-        for(int i=0;i<size;i++)
-        {
-            if(lar<m[nums[i]])
-            {
+        for(int i=0;i<size;i++){
+            if(lar<m[nums[i]]){
                 lar=m[nums[i]];
                 loc=nums[i];
             }
