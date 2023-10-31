5356aae8ae492ead9636534d4b20c5d80d6bb29e
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2023-11-01 00:50:11 +0530

    Time: 67 ms (8.24%), Space: 19.7 MB (88.63%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        int size=nums.size();
+        map<int,int> m;
+        
+        for(int i=0;i<size;i++)
+        {
+            m[nums[i]]=0;
+        }
+        
+        for(int i=0;i<size;i++)
+        {
+            m[nums[i]]++;
+        }
+        
+        int lar=0;
+        int loc;
+        
+        for(int i=0;i<size;i++)
+        {
+            if(lar<m[nums[i]])
+            {
+                lar=m[nums[i]];
+                loc=nums[i];
+            }
+        }
+        
+        return loc;
+    }
+};
