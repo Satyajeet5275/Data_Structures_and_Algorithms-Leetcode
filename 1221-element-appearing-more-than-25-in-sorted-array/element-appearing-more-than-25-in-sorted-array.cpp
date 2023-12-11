d3ecc134f8e1225a0c5b6edd2a093aa89a9182a9
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2023-12-11 11:56:55 +0530

    Time: 8 ms (78.76%) | Memory: 12.6 MB (92.26%) - LeetSync

--- 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.cpp
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.cpp
@@ -1,17 +1,5 @@
 class Solution {
 public:
-    // int findSpecialInteger(vector<int>& arr) {
-    //     int size=arr.size();
-    //     map<int,int> mp;
-    //     for(int i=0;i<size;i++){
-    //         mp[arr[i]]++;
-    //     }
-    //     for(auto it:mp){
-    //         float percent=((float)it.second/(float)size)*100.00;
-    //         if(percent>25.00) return it.first;       
-    //     }
-    //     return 0;
-    // }
      int findSpecialInteger(vector<int>& arr) {
         int size=arr.size();
         if(1.0>(25.00*size)/100) return arr[0];
