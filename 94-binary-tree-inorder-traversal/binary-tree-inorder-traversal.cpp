d278ddbad02b31d96ce65b1c9b4f365a379cc7e8
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2023-12-23 23:19:07 +0530

    Time: 4 ms (33.35%) | Memory: 8.6 MB (78.55%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -6,22 +6,47 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-void inorder(TreeNode *root,vector<int>& ans)
-{
-    if(root==NULL)  return;
-    
-    inorder(root->left,ans);
-    ans.push_back(root->val);
-    inorder(root->right,ans);
-}
+    void inorder(TreeNode* root, vector<int>& ans) {
+        if (root == NULL)
+            return;
+
+        inorder(root->left, ans);
+        ans.push_back(root->val);
+        inorder(root->right, ans);
+    }
+    void morrisInorder(TreeNode* root, vector<int>& ans) {
+        TreeNode *cur = root;
+        while (cur != NULL) {
+            if(cur->left == NULL) {
+                ans.push_back(cur->val);
+                cur=cur->right;
+            }
+            else {
+                TreeNode *prev=cur->left;
+                while(prev->right && prev->right != cur) {
+                    prev=prev->right;
+                }
+                if(prev->right == NULL) {
+                    prev->right = cur;
+                    cur = cur->left;
+                }
+                else {
+                    prev->right = NULL;
+                    ans.push_back(cur->val);
+                    cur = cur->right;
+                }
+            }
+        }
+    }
     vector<int> inorderTraversal(TreeNode* root) {
-         vector<int> ans;
-        inorder(root,ans);
+        vector<int> ans;
+        morrisInorder(root, ans);
         return ans;
     }
 };
