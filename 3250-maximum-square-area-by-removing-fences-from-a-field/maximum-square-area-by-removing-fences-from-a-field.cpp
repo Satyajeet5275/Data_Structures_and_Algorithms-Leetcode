edc18f6f9f9f6c60e07fa3fabcae580978a2a3e6
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2023-12-24 22:17:11 +0530

    Time: 1815 ms (30.00%) | Memory: 441.4 MB (30.00%) - LeetSync

--- 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.cpp
+++ 3250-maximum-square-area-by-removing-fences-from-a-field/maximum-square-area-by-removing-fences-from-a-field.cpp
@@ -1,35 +1,32 @@
 class Solution {
 public:
+    int mod=1e9+7;
     int maximizeSquareArea(int m, int n, vector<int>& h, vector<int>& v) {
-        int mod=1e9+7;
-        unordered_set<int>s1,s2;
-        h.push_back(1);
-        h.push_back(m);
         v.push_back(1);
         v.push_back(n);
+        h.push_back(1);
+        h.push_back(m);
         sort(h.begin(),h.end());
         sort(v.begin(),v.end());
-        for(int i=0;i<h.size();i++) {
-            for(int j=i+1;j<h.size();j++) {
-                s1.insert(h[j]-h[i]);
+        unordered_set<int> s1,s2;
+        for(int i=0;i<h.size();i++){
+            for(int j=i+1;j<h.size();j++){
+                s1.insert((h[j]-h[i]));
             }
         }
-        for(int i=0;i<v.size();i++) {
-            for(int j=i+1;j<v.size();j++) {
-                s2.insert(v[j]-v[i]);
+        for(int i=0;i<v.size();i++){
+            for(int j=i+1;j<v.size();j++){
+                s2.insert((v[j]-v[i]));
             }
         }
         long long ans=0;
-        for(auto i:s1) {
-            if(s2.find(i)!=s2.end()) {
-                long long k=(long long)i*i;
-                if(k>ans)
-                    ans=k;
-            } 
+        for(int x:s1){
+            if(s2.find(x)!=s2.end()){
+                long long area=(long long)x*(long long)x;
+                ans=max(ans,area);
+            }
         }
-        if(ans==0)
-            return -1;
-        ans=ans%mod;
-        return ans;
+        if(ans==0) return -1;
+        return (int)(ans%mod);
     }
 };
