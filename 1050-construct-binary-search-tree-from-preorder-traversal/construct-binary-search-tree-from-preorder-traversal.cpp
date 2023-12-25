75d2c4f93ac99948e3277e392f19b22bf681b47a
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2023-12-26 00:06:16 +0530

    Time: 7 ms (21.86%) | Memory: 14.2 MB (22.19%) - LeetSync

--- 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1050-construct-binary-search-tree-from-preorder-traversal/construct-binary-search-tree-from-preorder-traversal.cpp
@@ -11,24 +11,33 @@
  */
 class Solution {
 public:
-    TreeNode* build(vector<int>& bst,int bstart,int bend,vector<int>& pre,int pstart,int pend,map<int,int> &mp){
-        if(pstart>pend || bstart>bend) return NULL;
+    // TreeNode* build(vector<int>& bst,int bstart,int bend,vector<int>& pre,int pstart,int pend,map<int,int> &mp){
+    //     if(pstart>pend || bstart>bend) return NULL;
 
-        TreeNode* root=new TreeNode(pre[pstart]);
-        int inRoot=mp[pre[pstart]];
-        int remLeft=inRoot-bstart;
-        root->left=build(bst,bstart,inRoot-1,pre,pstart+1,pstart+remLeft,mp);
-        root->right=build(bst,inRoot+1,bend,pre,pstart+1+remLeft,pend,mp);
+    //     TreeNode* root=new TreeNode(pre[pstart]);
+    //     int inRoot=mp[pre[pstart]];
+    //     int remLeft=inRoot-bstart;
+    //     root->left=build(bst,bstart,inRoot-1,pre,pstart+1,pstart+remLeft,mp);
+    //     root->right=build(bst,inRoot+1,bend,pre,pstart+1+remLeft,pend,mp);
+    //     return root;
+    // }
+    TreeNode* build(vector<int>& pre,int &i,int bound) {
+        if(i>=pre.size() || pre[i]>bound) return NULL;
+        TreeNode *root=new TreeNode(pre[i++]);
+        root->left=build(pre,i,root->val);
+        root->right=build(pre,i,bound);
         return root;
     }
     TreeNode* bstFromPreorder(vector<int>& pre) {
-        vector<int> bst=pre;
-        sort(bst.begin(),bst.end());
-        map<int,int> mp;
-        for(int i=0;i<pre.size();i++) {
-             mp[bst[i]]=i;
-        }
-        return build(bst,0,bst.size()-1,pre,0,pre.size()-1,mp);
+        // vector<int> bst=pre;
+        // sort(bst.begin(),bst.end());
+        // map<int,int> mp;
+        // for(int i=0;i<pre.size();i++) {
+        //      mp[bst[i]]=i;
+        // }
+        // return build(bst,0,bst.size()-1,pre,0,pre.size()-1,mp);
+        int i=0;
+        return build(pre,i,INT_MAX);
     }
 };
 
