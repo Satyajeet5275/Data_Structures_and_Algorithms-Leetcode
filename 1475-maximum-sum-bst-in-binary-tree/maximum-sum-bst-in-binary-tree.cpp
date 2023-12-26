596db9d99f16b53c90b9423c958dd287be6eafb1
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2023-12-26 21:36:51 +0530

    Time: 151 ms (72.95%) | Memory: 114.8 MB (60.83%) - LeetSync

--- 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
+++ 1475-maximum-sum-bst-in-binary-tree/maximum-sum-bst-in-binary-tree.cpp
@@ -1,14 +1,3 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class NodeData{
     public:
     int minVal,maxVal,sum;
@@ -25,7 +14,6 @@ public:
         if(!root){
             return NodeData(INT_MAX,INT_MIN,0);
         }
-        // if(root->left==NULL && root->right==NULL) return NodeData(root->val,root->val,root->val);
         NodeData left=check(root->left);
         NodeData right=check(root->right);
         if(left.maxVal<root->val && right.minVal>root->val){
