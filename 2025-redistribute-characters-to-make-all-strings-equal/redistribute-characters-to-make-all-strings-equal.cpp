9e8faee898faaf2d9df9847a967c8ed7361ee709
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2023-12-30 20:58:24 +0530

    Time: 33 ms (5.06%) | Memory: 12.7 MB (10.12%) - LeetSync

--- 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
+++ 2025-redistribute-characters-to-make-all-strings-equal/redistribute-characters-to-make-all-strings-equal.cpp
@@ -3,16 +3,17 @@ public:
     bool makeEqual(vector<string>& words) {
         int n=words.size(),totalChars=0;
         if(n<=1) return true;
-        for(string str:words){
-            totalChars+=str.length();
-        }
-        if(totalChars%n!=0) return false;
         map<char,int> mp;
         for(string str:words){
+            totalChars+=str.length();
             for(char ch:str){
                 mp[ch]++;
             }
         }
+        if(totalChars%n!=0) return false;
+        // for(string str:words){
+            
+        // }
         for(pair<char,int> x:mp){
             if(x.second%n!=0) return false;
         }
