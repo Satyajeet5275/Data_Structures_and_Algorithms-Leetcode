187cb3084fab30ea2d8aebc6665a1d6be68de68d
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2024-01-12 12:49:08 +0530

    Time: 114 ms (43.81%) | Memory: 67.1 MB (32.54%) - LeetSync

---
+++ 456-132-pattern/132-pattern.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        stack<int> st;
+        int third=INT_MIN;
+        for(int i=nums.size()-1;i>=0;i--){
+            if(third>nums[i]) return true;
+            while(!st.empty() && st.top()<nums[i]){
+                third=st.top();
+                st.pop();
+            }
+            st.push(nums[i]);
+        }
+        return false;
+    }
+};
