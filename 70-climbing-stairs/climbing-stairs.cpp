32d7dce5899abd30667b67e7cfad5631a4ad9bfe
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2024-01-18 19:23:58 +0530

    Time: 2 ms (45.07%) | Memory: 6.6 MB (35.64%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int solve(int n,vector<int>& dp)
+    {
+        if(n==0)    return 1;
+        if(n<0)    return 0;
+        //DP Memoization
+        if(dp[n]!=-1)   return dp[n];
+
+        int left=solve(n-1,dp);
+        int right=solve(n-2,dp);
+        return dp[n]=left+right;
+    }
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
