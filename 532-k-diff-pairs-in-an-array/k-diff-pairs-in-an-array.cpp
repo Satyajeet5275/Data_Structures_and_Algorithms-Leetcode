189841a155e6ad9334d4bedf6dcecc1a554a094a
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2024-01-22 15:25:19 +0530

    Time: 63 ms (12.98%) | Memory: 18.8 MB (5.12%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     int findPairs(vector<int>& arr, int k) {
-        // sort(arr.begin(),arr.end());
         set<pair<int,int>> st;
         map<int,int> mp;
         for(int x:arr) mp[x]++;
@@ -12,8 +11,6 @@ public:
                         st.insert({arr[i],num});
                         cout<<arr[i]<<" "<<num<<endl;           
                 }
-                // num=arr[i]-(k);
-                // if(mp[num]>=1) st.insert({arr[i],num});
         }
         return st.size();
     }
