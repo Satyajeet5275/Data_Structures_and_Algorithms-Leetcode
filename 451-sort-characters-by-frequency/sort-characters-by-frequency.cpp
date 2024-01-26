65668a649ea97159194562dae81812e852dcca65
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2024-01-27 00:13:40 +0530

    Time: 111 ms (6.79%) | Memory: 9.8 MB (17.69%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    static bool compair(pair<int,char> a,pair<int,char> b) {
+        if(a.first==b.first) return a.second<b.second;
+        return a.first>b.first;
+    }
+    string frequencySort(string s) {
+        vector<pair<int,char>> arr(62,{0,'a'});
+        for(auto x:s){
+            cout<<x-'a'<<endl;
+            if(x>='a'){
+                arr[x-'a'].first++;
+                arr[x-'a'].second=x;
+            }
+            else if(x>='A'){
+                arr[26+x-'A'].first++;
+                arr[26+x-'A'].second=x;
+            }
+            else{
+                arr[52+x-'0'].first++;
+                arr[52+x-'0'].second=x;
+            }
+        }
+        sort(arr.begin(),arr.end(),compair);
+        string ans;
+        for(auto x:arr){
+            if(x.first>0){
+                string tmp=string(x.first,x.second);
+                ans+=tmp;
+            }
+        }
+        return ans;
+    }
+};
