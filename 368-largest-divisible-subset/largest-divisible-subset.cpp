ae3dad386dc6683fa967b823c9b34c1a70f39841
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2024-02-09 20:59:51 +0530

    Time: 2817 ms (5.10%) | Memory: 499.2 MB (5.01%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    vector<int> ans;
-    void solve(int ind,vector<int>& nums,vector<int> curr,vector<int>& dp){
+
+    void solve(int ind,vector<int>& nums,vector<int> curr,vector<int>& dp,vector<int>& ans){
         if(ind>=nums.size()){
             if(curr.size()>ans.size()) ans=curr;
             return ;
         }
         if(curr.size()==0){
             curr.push_back(nums[ind]);
-            solve(ind+1,nums,curr,dp);
+            solve(ind+1,nums,curr,dp,ans);
             curr.pop_back();
         }
         else if((int)curr.size()>dp[ind] && nums[ind]%curr.back()==0){
             dp[ind]=curr.size();
             curr.push_back(nums[ind]);
-            solve(ind+1,nums,curr,dp);
+            solve(ind+1,nums,curr,dp,ans);
             curr.pop_back();
         }
-        solve(ind+1,nums,curr,dp);
+        solve(ind+1,nums,curr,dp,ans);
     }
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         vector<int> dp(nums.size()+1,-1);
+        vector<int> ans;
         sort(nums.begin(),nums.end());
-        solve(0,nums,{},dp);
+        solve(0,nums,{},dp,ans);
         return ans;
     }
 };
