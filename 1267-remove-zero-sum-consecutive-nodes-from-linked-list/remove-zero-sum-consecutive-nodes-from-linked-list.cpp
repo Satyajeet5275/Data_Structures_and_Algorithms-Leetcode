8b9c153576d407feb3f74fa9a06a6f6203343bfb
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2024-03-12 15:10:08 +0530

    Time: 16 ms (6.48%) | Memory: 13.5 MB (64.65%) - LeetSync

--- 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
+++ 1267-remove-zero-sum-consecutive-nodes-from-linked-list/remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -1,3 +1,13 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* removeZeroSumSublists(ListNode* head) {
@@ -6,32 +16,30 @@ public:
             arr.push_back(head->val);
             head=head->next;
         }
-        
-        bool found = true;
-        while(found) {
-            found = false;
-            int n = arr.size();
-            for(int i = 0; i < n; i++){
-                int sum = 0;
-                for(int j = i; j < n; j++){
-                    sum += arr[j];
-                    if(sum == 0){
+        bool found=true;
+        while(found){
+            found=false;
+            int n=arr.size();
+            for(int i=0;i<n;i++){
+                int sum=0;
+                for(int j=i;j<n;j++){
+                    sum+=arr[j];
+                    if(sum==0){
                         arr.erase(arr.begin() + i, arr.begin() + j + 1);
-                        found = true;
+                        found=true;
                         break;
                     }
                 }
                 if(found) break;
-            }
+            }        
+        }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+        ListNode* n1=new ListNode(0);
+        head=n1;
+        for(auto x:arr){
+            head->next=new ListNode(x);
+            head=head->next;
         }
-        
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
-        for(auto x: arr) {
-            curr->next = new ListNode(x);
-            curr = curr->next;
-        }
-        
-        return dummy->next;
+        return n1->next; 
     }
 };
+
