d8f7ea8bc293379d0ba4f6d323638ec4976d99ec
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2024-10-01 11:19:02 +0530

    Time: 249 ms (5.16%), Space: 74.7 MB (27.78%) - LeetHub

---
+++ 1620-check-if-array-pairs-are-divisible-by-k/1620-check-if-array-pairs-are-divisible-by-k.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+        map<int,int> mp;
+        for(auto x:arr) mp[(x%k+k)%k]++;
+
+        for(auto x:arr){
+            int rem=(x%k+k)%k;
+            if(rem==0){
+                if(mp[rem]%2==1)    return false;
+            } 
+            else if(mp[rem]!=mp[k-rem]) return false;
+        }
+        return true;
+    }
+};
