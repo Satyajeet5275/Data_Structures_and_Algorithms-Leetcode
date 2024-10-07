70a628055f96852aad5712fb114225337b94c713
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2024-10-07 12:15:52 +0530

    Time: 11 ms (20.10%) | Memory: 10.1 MB (93.92%) - LeetSync

---
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minLength(string s) {
+        
+        while(s.find("AB")!=string::npos || s.find("CD")!=string::npos){
+            if(s.find("AB")!=string::npos) s.erase(s.find("AB"),2);
+            if(s.find("CD")!=string::npos) s.erase(s.find("CD"),2);
+        }
+        return s.length();
+    }
+};
