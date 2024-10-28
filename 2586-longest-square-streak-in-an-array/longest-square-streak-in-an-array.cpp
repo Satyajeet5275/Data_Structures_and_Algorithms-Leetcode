c60ad6bb76c2e0b2ec446db6fc09ee077050acd8
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2024-10-28 19:48:17 +0530

    Time: 84 ms (68.89%) | Memory: 109 MB (63.34%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
@@ -1,30 +1,22 @@
 class Solution {
 public:
-    int solve(int currNum, vector<int>& nums, vector<int>& dp) {
-        if (currNum >= dp.size()) return 0;
-        if (dp[currNum] != -1) return dp[currNum];
-        
-        long long square = (long long)currNum * currNum;
-        if (square >= dp.size()) return dp[currNum] = 0;
-        
-        auto it = lower_bound(nums.begin(), nums.end(), square);
-        if (it == nums.end() || *it != square) {
-            return dp[currNum] = 0;
-        }
-        
-        return dp[currNum] = 1 + solve(square, nums, dp);
-    }
-    
     int longestSquareStreak(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int maxVal = nums.back();
-        vector<int> dp(maxVal + 1, -1);
+        unordered_set<int> st;
+        for(int x: nums) st.insert(x);
+        int maxStreak = 0;
         
-        int maxStreak = 1;
-        for (int num : nums) {
-            maxStreak = max(maxStreak, 1 + solve(num, nums, dp));
+        for(auto x: nums) {
+            long long square = (long long)x * x;
+            int streak = 1;
+            
+            while(square <= INT_MAX && st.find((int)square) != st.end()) {
+                streak++;
+                square = (long long)square * square;
+            }
+            
+            maxStreak = max(maxStreak, streak);
         }
         
-        return maxStreak >= 2 ? maxStreak : -1;
+        return maxStreak == 1 ? -1 : maxStreak;
     }
 };
