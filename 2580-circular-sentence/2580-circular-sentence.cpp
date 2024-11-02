f2f2b353120ea33368981d7841358f9926df16a9
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2024-11-02 17:19:20 +0530

    Time: 0 ms (100%), Space: 8.2 MB (59.78%) - LeetHub

--- 2580-circular-sentence/2580-circular-sentence.cpp
+++ 2580-circular-sentence/2580-circular-sentence.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     bool isCircularSentence(string s) {
-        if(s[0]!=s[s.size()-1]) return false;
-        for(int i=0;i<s.size();i++){
-            if(s[i]==' '){
-                if(s[i-1]!=s[i+1]) return false;
+        if (s[0] != s[s.size() - 1])    return false;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i] == ' ') {
+                if (s[i - 1] != s[i + 1])
+                    return false;
             }
         }
         return true;
