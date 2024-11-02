a1b3b11bbe5bb7a4efc457eb76ca2a03eeff5092
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2024-11-02 17:12:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (31.30%) - LeetSync

---
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isCircularSentence(string s) {
+        if(s[0]!=s[s.size()-1]) return false;
+        for(int i=0;i<s.size();i++){
+            if(s[i]==' '){
+                if(s[i-1]!=s[i+1]) return false;
+            }
+        }
+        return true;
+    }
+};
