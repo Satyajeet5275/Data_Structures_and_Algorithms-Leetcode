0ed253c805407d1efce9e6773a04bc74bbc0543f
Satyajit Gaikwad <80042383+Satyajeet5275@users.noreply.github.com>
2024-11-03 13:23:55 +0530

    Time: 3 ms (7.41%) | Memory: 8.7 MB (16.05%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -5,7 +5,6 @@ public:
         for(int i=0;i<s.size();i++){
             if(tmp==goal) return true;
             tmp=tmp[s.size()-1]+tmp.substr(0,s.size()-1);
-            // cout<<tmp<<endl;
         }
         return false;
     }
